bd088baaddea856c59efe3e79b62449d374f57c1
Ryan Cragun <me@ryan.ec>
2014-11-04 10:37:50 -0800

    Add initial backup library

---
+++ lib/chef_backup.rb
@@ -0,0 +1,38 @@
+# Copyright:: Copyright (c) 2014 Opscode, Inc.
+#
+# All Rights Reserved
+
+require 'chef_backup/version'
+require 'chef_backup/data_map'
+require 'chef_backup/helpers'
+require 'chef_backup/backup'
+require 'chef_backup/restore'
+
+# ChefBackup factory returns an ChefBackup object
+module ChefBackup
+  def self.from_config(config)
+    type = config['private_chef']['backup']['strategy']
+    klass = type.to_s.split('_').map! { |w| w.capitalize }.join
+    const_get(klass).new(config)
+  rescue NoMethodError, NameError
+    msg = "Invalid strategy.  Please set the backup['strategy'] in "
+    msg << " /etc/opscode/chef-server.rb and run 'chef-server-ctl reconfigure'"
+    puts msg
+    exit 1
+  end
+end
+
+# No module inheritence makes DRY a dull boy
+# ChefRestore factory returns an ChefRestore object
+module ChefRestore
+  def self.from_config(config)
+    type = config['private_chef']['backup']['strategy']
+    klass = type.to_s.split('_').map! { |w| w.capitalize }.join
+    const_get(klass).new(config)
+  rescue NoMethodError, NameError
+    msg = 'Invalid strategy.  '
+    msg << "Please set the backup['strategy'] in /etc/opscode/chef-server.rb"
+    puts msg
+    exit 1
+  end
+end
---
+++ lib/chef_backup/version.rb
@@ -0,0 +1,4 @@
+# ChefBackup module
+module ChefBackup
+  VERSION = '0.0.1'
+end
