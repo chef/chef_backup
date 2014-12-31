13b96dbbce131a9ca671be7f4997ff5fa2550d9b
Ryan Cragun <me@ryan.ec>
2014-12-30 16:40:53 -0800

    Fix formatting on logger:

--- lib/chef_backup/logger.rb
+++ lib/chef_backup/logger.rb
@@ -1,40 +1,38 @@
 require 'highline'
 
 module ChefBackup
-# rubocop:disable IndentationWidth
-class Logger
-  # rubocop:enable IndentationWidth
-  def self.logger(logfile = nil)
-    @logger = nil if @logger && logfile && @logger.stdout != logfile
-    @logger ||= new(logfile)
-  end
+  class Logger
+    def self.logger(logfile = nil)
+      @logger = nil if @logger && logfile && @logger.stdout != logfile
+      @logger ||= new(logfile)
+    end
 
-  def self.log(msg, level = :info)
-    logger.log(msg, level)
-  end
+    def self.log(msg, level = :info)
+      logger.log(msg, level)
+    end
 
-  attr_accessor :stdout
+    attr_accessor :stdout
 
-  def initialize(logfile = nil)
-    @stdout = logfile || $stdout
-    @highline = HighLine.new($stdin, @stdout)
-  end
+    def initialize(logfile = nil)
+      @stdout = logfile || $stdout
+      @highline = HighLine.new($stdin, @stdout)
+    end
 
-  def log(msg, level = :info)
-    case level
-    when :warn
-      msg = "WARNING: #{msg}"
-      @stdout.puts( color? ? @highline.color(msg, :yellow) : msg)
-    when :error
-      msg = "ERROR: #{msg}"
-      @stdout.puts( color? ? @highline.color(msg, :red) : msg)
-    else
-      @stdout.puts(msg)
+    def log(msg, level = :info)
+      case level
+      when :warn
+        msg = "WARNING: #{msg}"
+        @stdout.puts( color? ? @highline.color(msg, :yellow) : msg)
+      when :error
+        msg = "ERROR: #{msg}"
+        @stdout.puts( color? ? @highline.color(msg, :red) : msg)
+      else
+        @stdout.puts(msg)
+      end
+    end
+
+    def color?
+      @stdout.tty?
     end
   end
-
-  def color?
-    @stdout.tty?
-  end
-end
 end
