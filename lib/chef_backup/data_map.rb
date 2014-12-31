58f87f97a5780e52ae679485ae034925c045da72
Ryan Cragun <me@ryan.ec>
2014-12-30 16:38:27 -0800

    Fix formatting on DataMap

--- lib/chef_backup/data_map.rb
+++ lib/chef_backup/data_map.rb
@@ -1,45 +1,47 @@
 require 'time'
 
 # DataMap class to store data about the data we're backing up
-class ChefBackup::DataMap
+class ChefBackup
+  class DataMap
 
-  class << self
-    def data_map
-      @data_map ||= new
+    class << self
+      def data_map
+        @data_map ||= new
+      end
+
+      def data_map=(data_map)
+        @data_map = data_map
+      end
     end
 
-    def data_map=(data_map)
-      @data_map = data_map
+    attr_accessor :strategy, :backup_time, :configs, :services
+
+    def initialize
+      @services = {}
+      @configs = {}
+      yield self if block_given?
+
+      @backup_time ||= Time.now.iso8601
+      @strategy ||= 'none'
     end
-  end
 
-  attr_accessor :strategy, :backup_time, :configs, :services
+    def add_service(service, data_dir)
+      @services[service] ||= {}
+      @services[service]['data_dir'] = data_dir
+    end
 
-  def initialize
-    @services = {}
-    @configs = {}
-    yield self if block_given?
+    def add_config(config, path)
+      @configs[config] ||= {}
+      @configs[config]['data_dir'] = path
+    end
 
-    @backup_time ||= Time.now.iso8601
-    @strategy ||= Time.now.iso8601
-  end
-
-  def add_service(service, data_dir)
-    @services[service] ||= {}
-    @services[service]['data_dir'] = data_dir
-  end
-
-  def add_config(config, path)
-    @configs[config] ||= {}
-    @configs[config]['data_dir'] = path
-  end
-
-  def manifest
-    {
-      'strategy' => strategy,
-      'backup_time' => backup_time,
-      'services' => services,
-      'configs' => configs
-    }
+    def manifest
+      {
+        'strategy' => strategy,
+        'backup_time' => backup_time,
+        'services' => services,
+        'configs' => configs
+      }
+    end
   end
 end
