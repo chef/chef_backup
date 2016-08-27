9f1f198fc88088c7def0a2466a680adfbfb8828f
Jeremy J. Miller <jm@chef.io>
2016-08-26 21:51:17 -0400

    added chef-manage addon

--- lib/chef_backup/helpers.rb
+++ lib/chef_backup/helpers.rb
@@ -10,6 +10,10 @@ module Helpers
   # rubocop:enable IndentationWidth
 
   SERVER_ADD_ONS = {
+    'chef-manage' => {
+      'config_file' => '/etc/chef-manage/manage.rb',
+      'ctl_command' => 'chef-manage-ctl'
+    },
     'opscode-manage' => {
       'config_file' => '/etc/opscode-manage/manage.rb',
       'ctl_command' => 'opscode-manage-ctl'
